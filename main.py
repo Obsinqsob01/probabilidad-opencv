114b18142905c4e62d5733f7f6f35384a913943f
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-26 11:33:27 -0600

    Added file generation of the array data

--- main.py
+++ main.py
@@ -5,12 +5,35 @@ import cv2
 imagen1 = cv2.imread("./yaktocat.png")
 imagen2 = cv2.imread("./octocat-de-los-muertos.jpg")
 
+img1 = cv2.resize(imagen1, (30, 30))
+img2 = cv2.resize(imagen2, (30, 30))
+
+# La cambia a blanco y negro
+imagen_gris1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+imagen_gris2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
 # Suma de las dos imagenes
-resultado = imagen1 + imagen2
+resultado = imagen_gris1 + imagen_gris2
+
+# Imprime el resultado de las imagenes sumadass
+print(resultado)
 
 # Mostramos la imagen
 cv2.imshow("Imagenes combinadas", resultado)
 
+# Guardamos la imagen 1
+f = open("resultado_imagen1.txt", "w+")
+f.write(str(imagen_gris1))
+
+# Guardamos la imagen 2
+f = open("resultado_imagen2.txt", "w+")
+f.write(str(imagen_gris2))
+
+# Guardamos la imagen 3
+f = open("resultado_imagen.txt", "w+")
+f.write(str(resultado))
+
 # Indicamos que se tiene que esperar a que el usuario teclee el numero 0
 # para el programa termine de ejecutarse
 cv2.waitKey(0)
+cv2.destroyAllWindows()
